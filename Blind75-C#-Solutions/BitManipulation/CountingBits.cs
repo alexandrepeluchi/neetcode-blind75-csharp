ef656d65a872445f30a469b429e7011e1ff5100f
Alexandre Peluchi <alexandrepeluchi@gmail.com>
2026-04-13 18:51:33 -0300

    Solução do Desafio CountingBits

---
+++ Blind75-C#-Solutions/BitManipulation/CountingBits.cs
@@ -0,0 +1,12 @@
+public class Solution {
+    public int[] CountBits(int n) {
+        int[] result = new int[n + 1];
+
+        for (int i = 1; i <= n; i++)
+        {
+            result[i] = result[i >> 1] + (i & 1);
+        }
+
+        return result;
+    }
+}
